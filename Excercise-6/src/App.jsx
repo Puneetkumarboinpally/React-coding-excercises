f2253f0035a19b2f791a75231692698eaf26a77c
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-07-01 18:21:12 +0100

    axios and html added

--- Excercise-6/src/App.jsx
+++ Excercise-6/src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <div>
+        <h1>Profile card</h1>
+        <div>
+          <img src="" />
+          <h2></h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <button>More info</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
