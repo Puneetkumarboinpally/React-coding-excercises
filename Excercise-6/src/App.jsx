f6615ccf6ea24e51c1c0c9910072239366dfe3c2
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-07-01 21:46:55 +0100

    final commit

--- Excercise-6/src/App.jsx
+++ Excercise-6/src/App.jsx
@@ -17,18 +17,28 @@ const App = () => {
   }, []);
 
   return (
-    <div className="p-8 grid sm:grid-cols-2  lg:grid-cols-4 gap-5">
-      {task.map((elem, idx) => (
+    <div className="bg-gradient-to-r from-blue-500 to-purple-600 p-8 grid sm:grid-cols-2 lg:grid-cols-5 gap-5">
+      {task.map((elem) => (
         <div
-          key={idx}
-          className="p-4 bg-white/10 backdrop-blur-md border border-white/20 rounded-2xl text-center"
+          key={elem.id}
+          className="flex flex-col justify-between p-4 bg-white/30 backdrop-blur-md border border-white/40 rounded-2xl shadow-2xl text-center"
         >
           <img
             src={elem.download_url}
-            className="h-60 w-full object-cover rounded-xl "
+            className="h-52 w-full object-cover rounded-xl transition-all duration-300 hover:scale-105"
           />
-          <h2>{elem.author}</h2>
-          <p>Lorem ipsum dolor, sie amet consectetur adipisicing elit.</p>
+          <h2 className="my-2 text-lg font-semibold">Author: {elem.author}</h2>
+          <p className="mb-4">
+            Lorem ipsum dolor, sie amet consectetur adipisicing elit.
+          </p>
+          <a
+            href={elem.url}
+            target="_blank"
+            rel="noreferrer"
+            className="block py-2 px-4 w-full font-semibold rounded-lg bg-white/30 shadow-2xl transition-all duration-300 hover:-translate-y-1"
+          >
+            More info
+          </a>
         </div>
       ))}
     </div>
