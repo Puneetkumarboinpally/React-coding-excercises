93623ec144aa664176652814103a9a5a017f4ca0
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-07-05 23:35:36 +0100

    react added

--- Excercise-7/src/App.jsx
+++ Excercise-7/src/App.jsx
@@ -1,10 +1,36 @@
+import { useEffect, useState } from "react";
+
 const App = () => {
+  const [darkMode, setDarkMode] = useState(() => {
+    return JSON.parse(localStorage.getItem("darkMode")) || false;
+  });
+  useEffect(() => {
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+  }, [darkMode]);
   return (
-    <div className="flex justify-center items-center bg-gray-600 h-screen">
+    <div
+      className={
+        darkMode
+          ? "bg-gray-900 text-white h-screen flex justify-center items-center"
+          : "bg-gray-200 text-black h-screen flex justify-center items-center"
+      }
+    >
       <div className="bg-white/10 border border-white/20 p-8 rounded-xl text-center shadow-md">
-        <h1 className="text-lg font-bold text-white capitalize mb-2">Dark mode with local Storage</h1>
-        <button className="px-4 py-2 bg-white mx-4 rounded font-semibold cursor-pointer shadow-lg transition all duration-300 hover:translate-y-[-2px]">Light Mode</button>
-        <button className="px-4 py-2 bg-white rounded font-semibold cursor-pointer shadow-lg transition all duration-300 hover:translate-y-[-2px]">Dark Mode</button>
+        <h1 className="text-lg font-bold text-white capitalize mb-2">
+          Dark mode with local Storage
+        </h1>
+        <button
+          onClick={() => setDarkMode(false)}
+          className="px-4 py-2 bg-white mx-4 rounded font-semibold cursor-pointer shadow-lg transition all duration-300 hover:translate-y-[-2px]"
+        >
+          Light Mode
+        </button>
+        <button
+          onClick={() => setDarkMode(true)}
+          className="px-4 py-2 bg-white rounded font-semibold cursor-pointer shadow-lg transition all duration-300 hover:translate-y-[-2px]"
+        >
+          Dark Mode
+        </button>
       </div>
     </div>
   );
