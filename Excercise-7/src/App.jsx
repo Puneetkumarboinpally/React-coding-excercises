9fb337c16991bfc9531769ebad50078e31254ebf
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-07-05 23:01:00 +0100

    html added

--- Excercise-7/src/App.jsx
+++ Excercise-7/src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="flex justify-center items-center bg-gray-600 h-screen">
+      <div className="bg-white/10 border border-white/20 p-8 rounded-xl text-center shadow-md">
+        <h1 className="text-lg font-bold text-white capitalize mb-2">Dark mode with local Storage</h1>
+        <button className="px-4 py-2 bg-white mx-4 rounded font-semibold cursor-pointer shadow-lg transition all duration-300 hover:translate-y-[-2px]">Light Mode</button>
+        <button className="px-4 py-2 bg-white rounded font-semibold cursor-pointer shadow-lg transition all duration-300 hover:translate-y-[-2px]">Dark Mode</button>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
