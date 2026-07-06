685be763f80c58d22c0945fa0b770f63330ec529
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-07-06 20:23:44 +0100

    final commit updated

--- Excercise-8/src/App.jsx
+++ Excercise-8/src/App.jsx
@@ -3,8 +3,8 @@ import { Sun, Moon } from "lucide-react";
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(() => {
-    const stored = JSON.parse(localStorage.getItem("darkMode"));
-    return stored ? stored : false;
+    const stored = localStorage.getItem("darkMode");
+    return stored ? JSON.parse(stored) : false;
   });
   useEffect(() => {
     localStorage.setItem("darkMode", JSON.stringify(darkMode));
@@ -18,20 +18,25 @@ const App = () => {
       >
         <div className="flex justify-between items-center w-64">
           <h1 className="font-semibold text-lg">Background changer</h1>
-          <div
+          <button
+            aria-label="Toggle dark mode"
             onClick={() => setDarkMode((prev) => !prev)}
             className="w-12 h-8 text-black flex items-center bg-white/10 border-2 border-white/20 p-1 rounded-full cursor-pointer "
           >
             <div
-              className={`w-5 h-5 flex items-center justify-center rounded-full transition-all duration-700 ${
+              className={`w-5 h-5 flex items-center justify-center rounded-full transition-all duration-500 ease-in-out ${
                 darkMode
                   ? "translate-x-4 rotate-90 bg-yellow-300"
                   : "translate-x-0 rotate-0 bg-blue-900"
               }`}
             >
-              {darkMode ? <Sun size={16} /> : <Moon size={16} className="text-white" />}
+              {darkMode ? (
+                <Sun size={16} />
+              ) : (
+                <Moon size={16} className="text-white" />
+              )}
             </div>
-          </div>
+          </button>
         </div>
       </div>
     </div>
