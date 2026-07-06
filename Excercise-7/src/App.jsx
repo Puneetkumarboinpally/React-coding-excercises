234337b73aa1009ecca8715d733c382250a117b9
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-07-06 01:50:34 +0100

    final commit

--- Excercise-7/src/App.jsx
+++ Excercise-7/src/App.jsx
@@ -1,35 +1,36 @@
 import { useEffect, useState } from "react";
+import { Sun, Moon } from "lucide-react";
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(() => {
-    return JSON.parse(localStorage.getItem("darkMode")) || false;
+    const saved = localStorage.getItem("darkMode");
+    return saved ? JSON.parse(saved) : false;
   });
   useEffect(() => {
     localStorage.setItem("darkMode", JSON.stringify(darkMode));
   }, [darkMode]);
   return (
     <div
-      className={
-        darkMode
-          ? "bg-gray-900 text-white h-screen flex justify-center items-center"
-          : "bg-gray-200 text-black h-screen flex justify-center items-center"
-      }
+      className={`${
+        darkMode ? "bg-gray-900 text-black" : "bg-gray-200 text-black"
+      } h-screen flex justify-center items-center`}
     >
-      <div className="bg-white/10 border border-white/20 p-8 rounded-xl text-center shadow-md">
-        <h1 className="text-lg font-bold text-white capitalize mb-2">
+      <div
+        className={`${
+          darkMode
+            ? "bg-white/10 border border-white/20 shadow-md"
+            : "bg-black/10 border border-black/20 shadow-md"
+        } 
+          p-8 rounded-xl text-center`}
+      >
+        <h1 className="text-lg font-bold text-blue-500 capitalize">
           Dark mode with local Storage
         </h1>
         <button
-          onClick={() => setDarkMode(false)}
-          className="px-4 py-2 bg-white mx-4 rounded font-semibold cursor-pointer shadow-lg transition all duration-300 hover:translate-y-[-2px]"
+          onClick={() => setDarkMode((prev) => !prev)}
+          className="py-2 px-5 bg-red-400 rounded-full font-semibold shadow-lg transition-all duration-300 hover:-translate-y-1"
         >
-          Light Mode
-        </button>
-        <button
-          onClick={() => setDarkMode(true)}
-          className="px-4 py-2 bg-white rounded font-semibold cursor-pointer shadow-lg transition all duration-300 hover:translate-y-[-2px]"
-        >
-          Dark Mode
+          {darkMode ? <Sun size={20} /> : <Moon size={20} />}
         </button>
       </div>
     </div>
