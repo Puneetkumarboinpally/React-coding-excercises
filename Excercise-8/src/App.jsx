8168f38acf16e69d4d414545a1af99dcc2341609
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-07-06 19:46:28 +0100

    final commit

--- Excercise-8/src/App.jsx
+++ Excercise-8/src/App.jsx
@@ -7,24 +7,30 @@ const App = () => {
     return stored ? stored : false;
   });
   useEffect(() => {
-    localStorage.setItem(darkMode, JSON.stringify(darkMode));
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
   }, [darkMode]);
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-800">
-      <div className="flex p-4 bg-white/10 border-2 border-white/20 shadow-lg rounded-xl">
+    <div
+      className={`flex justify-center items-center h-screen transition-all duration-300 ${darkMode ? "bg-gray-700" : "bg-gray-200"}`}
+    >
+      <div
+        className={`flex p-4 ${darkMode ? "bg-white/10 border-2 border-white/20 text-white " : "bg-black/30 border-2 border-black/20"} shadow-lg rounded-xl`}
+      >
         <div className="flex justify-between items-center w-64">
-          <h1 className="text-white font-semibold text-lg">
-            Background changer
-          </h1>
+          <h1 className="font-semibold text-lg">Background changer</h1>
           <div
             onClick={() => setDarkMode((prev) => !prev)}
-            className="w-14 h-8 flex items-center bg-white/10 border-2 border-white/20 p-1 rounded-full "
+            className="w-12 h-8 text-black flex items-center bg-white/10 border-2 border-white/20 p-1 rounded-full cursor-pointer "
           >
-            <button
-              className={`w-6 h-6 flex items-center bg-blue-300 p-1 rounded-full transition-all duration-500 transform ${darkMode ? "translate-x-5 rotate-90 bg-yellow-300" : "translate-x-0 rotate-0"}`}
+            <div
+              className={`w-5 h-5 flex items-center justify-center rounded-full transition-all duration-700 ${
+                darkMode
+                  ? "translate-x-4 rotate-90 bg-yellow-300"
+                  : "translate-x-0 rotate-0 bg-blue-900"
+              }`}
             >
-              {darkMode ? <Sun /> : <Moon />}
-            </button>
+              {darkMode ? <Sun size={16} /> : <Moon size={16} className="text-white" />}
+            </div>
           </div>
         </div>
       </div>
